10feafd184f2c19e7ebc899ff97709e590d5769a
Qihun Yang <yangqh29@mail2.sysu.edu.cn>
2023-05-12 20:08:36 +0800

    Ready to work
    this function is ready to work, but it still needs to add the newest tunning parameters.

--- Twohit.R
+++ Twohit.R
@@ -1,5 +1,6 @@
-Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC", 
-                     c2 = 2, c3 = 2.01, c_n = ?, intercept = TRUE) 
+Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5,
+                     HDIC_Type_L = "HDBIC", HDIC_Type_D = "HDBIC",
+                     c2 = 2, c3 = 2.01, intercept = TRUE) 
 {
 ###############################1.First step Ohit################################
   
@@ -28,52 +29,52 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
   }
   dy = y - mean(y)
   dX = apply(X, 2, function(x) x - mean(x))
-  Jhat = sigma2hat = rep(0, KLn)
-  XJhat = matrix(0, n, KLn)
+  Jhat_L = sigma2hat = rep(0, KLn)
+  XJhat_L = matrix(0, n, KLn)
   RL = as.matrix(dy)
   xnorms = sqrt(colSums((dX)^2))
   aSSE = (abs(t(RL) %*% dX)/xnorms)
-  Jhat[1] = which.max(aSSE)
-  XJhat[, 1] = (dX[, Jhat[1]]/sqrt(sum((dX[, Jhat[1]])^2)))
-  RL = RL - XJhat[, 1] %*% t(XJhat[, 1]) %*% RL
+  Jhat_L[1] = which.max(aSSE)
+  XJhat_L[, 1] = (dX[, Jhat_L[1]]/sqrt(sum((dX[, Jhat_L[1]])^2)))
+  RL = RL - XJhat_L[, 1] %*% t(XJhat_L[, 1]) %*% RL
   sigma2hat[1] = mean(RL^2)
   if (KLn > 1) {
     for (k in 2:KLn) {
       aSSE = (abs(t(RL) %*% dX)/xnorms)
-      aSSE[Jhat[1:(k - 1)]] = 0
-      Jhat[k] = which.max(aSSE)
-      rq = dX[, Jhat[k]] - XJhat[, 1:(k - 1)] %*%
-        t(XJhat[, 1:(k - 1)]) %*% dX[, Jhat[k]]
-      XJhat[, k] = (rq/sqrt(sum((rq)^2)))
-      RL = RL - XJhat[, k] %*% t(XJhat[, k]) %*% RL
+      aSSE[Jhat_L[1:(k - 1)]] = 0
+      Jhat_L[k] = which.max(aSSE)
+      rq = dX[, Jhat_L[k]] - XJhat_L[, 1:(k - 1)] %*%
+        t(XJhat_L[, 1:(k - 1)]) %*% dX[, Jhat_L[k]]
+      XJhat_L[, k] = (rq/sqrt(sum((rq)^2)))
+      RL = RL - XJhat_L[, k] %*% t(XJhat_L[, k]) %*% RL
       sigma2hat[k] = mean(RL^2)
     }
   }
   
   ##################################1.2.HDIC_L##################################
   #need sigma2hat and Jhat from step 1
-  if ((HDIC_Type != "HDAIC") & (HDIC_Type != "HDBIC") & (HDIC_Type != 
+  if ((HDIC_Type_L != "HDAIC") & (HDIC_Type_L != "HDBIC") & (HDIC_Type_L != 
                                                          "HDHQ")) 
-    stop("HDIC_Type should be \"HDAIC\", \"HDBIC\" or \"HDHQ\"")
-  if (HDIC_Type == "HDAIC") 
+    stop("HDIC_Type_L should be \"HDAIC\", \"HDBIC\" or \"HDHQ\"")
+  if (HDIC_Type_L == "HDAIC") 
     omega_n = c2
-  if (HDIC_Type == "HDBIC") 
+  if (HDIC_Type_L == "HDBIC") 
     omega_n = log(n)
-  if (HDIC_Type == "HDHQ") 
+  if (HDIC_Type_L == "HDHQ") 
     omega_n = c3 * log(log(n))
-  hdic = (n * log(sigma2hat)) + ((1:KLn) * omega_n * (log(pL)))
-  kln_hat = which.min(hdic)
-  benchmark = hdic[kln_hat]
+  hdic_L = (n * log(sigma2hat)) + ((1:KLn) * omega_n * (log(pL)))
+  kln_hat = which.min(hdic_L)
+  benchmark = hdic_L[kln_hat]
   # Outpue of HDIC
-  J_HDIC = sort(Jhat[1:kln_hat])
+  J_HDIC_L = sort(Jhat_L[1:kln_hat])
   
   ##################################1.3.Trim_L##################################
   # Initialize output of Trim
-  J_Trim = Jhat[1:kln_hat]
+  J_Trim_L = Jhat_L[1:kln_hat]
   trim_pos = rep(0, kln_hat)
   if (kln_hat > 1) {
     for (l in 1:(kln_hat - 1)) {
-      JDrop1 = J_Trim[-l]
+      JDrop1 = J_Trim_L[-l]
       fit = lm(dy ~ . - 1, data = data.frame(dX[, JDrop1]))
       RLDrop1 = fit$residuals
       HDICDrop1 = (n * log(mean(RLDrop1^2))) + ((kln_hat - 
@@ -82,17 +83,17 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
         trim_pos[l] = 1
     }
     trim_pos[kln_hat] = 1
-    J_Trim = J_Trim[which(trim_pos == 1)]
+    J_Trim_L = J_Trim_L[which(trim_pos == 1)]
   }
   # Outpue of Trim
-  J_Trim = sort(J_Trim)
+  J_Trim_L = sort(J_Trim_L)
   
   # Prepare the results
-  X_HDIC = as.data.frame(as.matrix(X[, J_HDIC]))
-  X_Trim = as.data.frame(as.matrix(X[, J_Trim]))
+  X_HDIC = as.data.frame(as.matrix(X[, J_HDIC_L]))
+  X_Trim = as.data.frame(as.matrix(X[, J_Trim_L]))
   X = data.frame(X)
-  colnames(X_HDIC) = names(X)[J_HDIC]
-  colnames(X_Trim) = names(X)[J_Trim]
+  colnames(X_HDIC) = names(X)[J_HDIC_L]
+  colnames(X_Trim) = names(X)[J_Trim_L]
   if (intercept == TRUE) {
     fit_HDIC = lm(y ~ ., data = X_HDIC)
     fit_Trim = lm(y ~ ., data = X_Trim)
@@ -101,16 +102,16 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
     fit_HDIC = lm(y ~ . - 1, data = X_HDIC)
     fit_Trim = lm(y ~ . - 1, data = X_Trim)
   }
-  betahat_HDIC = summary(fit_HDIC)
-  betahat_Trim = summary(fit_Trim)
+  betahat_HDIC_L = summary(fit_HDIC)
+  betahat_Trim_L = summary(fit_Trim)
 
   #############################1.4.Obtain residuals#############################
   # projection matrix H_{L,\hat{N}_{L,n}}
   if (intercept == TRUE) {
-    A <- cbind(matrix(1,n,1), dX[, result[["J_Trim"]]])
+    A <- cbind(matrix(1,n,1), dX[, result[["J_Trim_L"]]])
   }
   else {
-    A <- dX[, result[["J_Trim"]]]
+    A <- dX[, result[["J_Trim_L"]]]
   }
   proj <- A %*% solve(t(A) %*% A) %*% t(A)
   eta_hat2 <- ((diag(1,n) - proj) %*% dy)^2
@@ -141,52 +142,52 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
   }
   dr = r - mean(r)
   dZ = apply(Z, 2, function(x) x - mean(x))
-  Jhat = sigma2hat = rep(0, KDn)
-  ZJhat = matrix(0, n, KDn)
+  Jhat_D = sigma2hat = rep(0, KDn)
+  ZJhat_D = matrix(0, n, KDn)
   RD = as.matrix(dr)
   znorms = sqrt(colSums((dZ)^2))
   aSSE = (abs(t(RD) %*% dZ)/znorms)
-  Jhat[1] = which.max(aSSE)
-  ZJhat[, 1] = (dZ[, Jhat[1]]/sqrt(sum((dZ[, Jhat[1]])^2)))
-  RD = RD - ZJhat[, 1] %*% t(ZJhat[, 1]) %*% RD
+  Jhat_D[1] = which.max(aSSE)
+  ZJhat_D[, 1] = (dZ[, Jhat_D[1]]/sqrt(sum((dZ[, Jhat_D[1]])^2)))
+  RD = RD - ZJhat_D[, 1] %*% t(ZJhat_D[, 1]) %*% RD
   sigma2hat[1] = mean(RD^2)
   if (KDn > 1) {
     for (k in 2:KDn) {
       aSSE = (abs(t(RD) %*% dZ)/znorms)
-      aSSE[Jhat[1:(k - 1)]] = 0
-      Jhat[k] = which.max(aSSE)
-      rq = dZ[, Jhat[k]] - ZJhat[, 1:(k - 1)] %*%
-        t(ZJhat[, 1:(k - 1)]) %*% dZ[, Jhat[k]]
-      ZJhat[, k] = (rq/sqrt(sum((rq)^2)))
-      RD = RD - ZJhat[, k] %*% t(ZJhat[, k]) %*% RD
+      aSSE[Jhat_D[1:(k - 1)]] = 0
+      Jhat_D[k] = which.max(aSSE)
+      rq = dZ[, Jhat_D[k]] - ZJhat_D[, 1:(k - 1)] %*%
+        t(ZJhat_D[, 1:(k - 1)]) %*% dZ[, Jhat_D[k]]
+      ZJhat_D[, k] = (rq/sqrt(sum((rq)^2)))
+      RD = RD - ZJhat_D[, k] %*% t(ZJhat_D[, k]) %*% RD
       sigma2hat[k] = mean(RD^2)
     }
   }
   
   ##################################2.2.HDIC_D##################################
   #need sigma2hat and Jhat from step 1
-  if ((HDIC_Type != "HDAIC") & (HDIC_Type != "HDBIC") & (HDIC_Type != 
+  if ((HDIC_Type_D != "HDAIC") & (HDIC_Type_D != "HDBIC") & (HDIC_Type_D != 
                                                          "HDHQ")) 
-    stop("HDIC_Type should be \"HDAIC\", \"HDBIC\" or \"HDHQ\"")
-  if (HDIC_Type == "HDAIC") 
+    stop("HDIC_Type_D should be \"HDAIC\", \"HDBIC\" or \"HDHQ\"")
+  if (HDIC_Type_D == "HDAIC") 
     omega_n = c2
-  if (HDIC_Type == "HDBIC") 
+  if (HDIC_Type_D == "HDBIC") 
     omega_n = log(n)
-  if (HDIC_Type == "HDHQ") 
+  if (HDIC_Type_D == "HDHQ") 
     omega_n = c3 * log(log(n))
-  hdic = (n * log(sigma2hat)) + ((1:KDn) * omega_n * (log(pD)))
-  kdn_hat = which.min(hdic)
-  benchmark = hdic[kdn_hat]
-  # Outpue of HDIC
-  J_HDIC = sort(Jhat[1:kdn_hat])
+  hdic_D = (n * log(sigma2hat)) + ((1:KDn) * omega_n * (log(pD)))
+  kdn_hat = which.min(hdic_D)
+  benchmark = hdic_D[kdn_hat]
+  # Output of HDIC
+  J_HDIC_D = sort(Jhat_D[1:kdn_hat])
   
   ##################################2.3.Trim_D##################################
   # Initialize output of Trim
-  J_Trim = Jhat[1:kdn_hat]
+  J_Trim_D = Jhat_D[1:kdn_hat]
   trim_pos = rep(0, kdn_hat)
   if (kdn_hat > 1) {
     for (l in 1:(kdn_hat - 1)) {
-      JDrop1 = J_Trim[-l]
+      JDrop1 = J_Trim_D[-l]
       fit = lm(dr ~ . - 1, data = data.frame(dZ[, JDrop1]))
       RDDrop1 = fit$residuals
       HDICDrop1 = (n * log(mean(RDDrop1^2))) + ((kdn_hat - 
@@ -195,17 +196,17 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
         trim_pos[l] = 1
     }
     trim_pos[kdn_hat] = 1
-    J_Trim = J_Trim[which(trim_pos == 1)]
+    J_Trim_D = J_Trim_D[which(trim_pos == 1)]
   }
   # Outpue of Trim
-  J_Trim = sort(J_Trim)
+  J_Trim_D = sort(J_Trim_D)
   
   # Prepare the results
-  Z_HDIC = as.data.frame(as.matrix(Z[, J_HDIC]))
-  Z_Trim = as.data.frame(as.matrix(Z[, J_Trim]))
+  Z_HDIC = as.data.frame(as.matrix(Z[, J_HDIC_D]))
+  Z_Trim = as.data.frame(as.matrix(Z[, J_Trim_D]))
   Z = data.frame(Z)
-  colnames(Z_HDIC) = names(Z)[J_HDIC]
-  colnames(Z_Trim) = names(Z)[J_Trim]
+  colnames(Z_HDIC) = names(Z)[J_HDIC_D]
+  colnames(Z_Trim) = names(Z)[J_Trim_D]
   if (intercept == TRUE) {
     fit_HDIC = lm(y ~ ., data = Z_HDIC)
     fit_Trim = lm(y ~ ., data = Z_Trim)
@@ -214,13 +215,13 @@ Twotrim <- function (X, Z, y, KLn = NULL, KDn = NULL, c1 = 5, HDIC_Type = "HDBIC
     fit_HDIC = lm(y ~ . - 1, data = Z_HDIC)
     fit_Trim = lm(y ~ . - 1, data = Z_Trim)
   }
-  betahat_HDIC = summary(fit_HDIC)
-  betahat_Trim = summary(fit_Trim)
+  betahat_HDIC_D = summary(fit_HDIC)
+  betahat_Trim_D = summary(fit_Trim)
   
-  
-  
-  return(list(n = n, pL = pL, pD = pD, KLn = KLn, KDn = KDn, J_OGA = Jhat, 
-              HDIC = hdic, 
-              J_HDIC = J_HDIC, J_Trim = J_Trim, betahat_HDIC = betahat_HDIC, 
-              betahat_Trim = betahat_Trim))
+  return(list(n = n, pL = pL, pD = pD, KLn = KLn, KDn = KDn, J_OGA_L = Jhat_L,
+              J_OGA_D = Jhat_D, HDIC_L = hdic_L, HDIC_D = hdic_D,
+              J_HDIC_L = J_HDIC_L, J_HDIC_D = J_HDIC_D,
+              J_Trim_L = J_Trim_L, J_Trim_D = J_Trim_D,
+              betahat_HDIC_L = betahat_HDIC_L, betahat_HDIC_D = betahat_HDIC_D,
+              betahat_Trim_L = betahat_Trim_L, betahat_Trim_D = betahat_Trim_D))
 }
